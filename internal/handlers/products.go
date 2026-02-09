57835727137ec51f7d656331aafb28d6cc8c4e1f
hossei-cyber <hussayaqobzai@gmail.com>
2026-02-09 22:22:55 +0100

    feat!: restructure code with internal and pkg packages, moved handler functions to internal/handlers and utilities to pkg/

---
+++ internal/handlers/products.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/robinlieb/devops-lecture-project-2026/internal/models"
+	"github.com/robinlieb/devops-lecture-project-2026/pkg/utils"
+)
+
+func ProductListHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	
+	jsonResponse, err := json.Marshal(models.Products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal Server Error"}`))
+		return
+	}
+	w.Write(jsonResponse)
+}
+func ProductDetailHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	idStr := r.PathValue("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, `{"error":"Product ID has wrong format"}`, http.StatusBadRequest)
+		return
+	}
+
+	product := utils.FindProductByID(models.Products, id)
+	if product == nil {
+		http.Error(w, `{"error":"Product not found"}`, http.StatusNotFound)
+		return
+	}
+
+	resp, err := json.Marshal(product)
+	if err != nil {
+		http.Error(w, `{"error":"Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(resp)
+}
+
+
